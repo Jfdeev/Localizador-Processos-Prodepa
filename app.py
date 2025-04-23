d003df850e9f500f059788eaa584f8034ead7c47
Jfdeev <jfrsoares161205@gmail.com>
2025-04-23 16:46:42 -0300

    chore: more one paramneter of export

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ st.dataframe(df)
 def exportar_pdf(df: pd.DataFrame) -> bytes:
     # 1) Escolha aqui as colunas que quer no PDF:
     cols_export = ['PAE','CLIENTE','Andamento','Status contratual',
-                   'Vigência Início','Vigência Término']
+                   'Vigência Início','Vigência Término', 'Valor global Atual',]
     # Se alguma não existir, cai pra todas:
     if not set(cols_export).issubset(df.columns):
         df_export = df.copy()
